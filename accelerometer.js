1a4f225c5f369bcfd57464fe4850d7a6dabfcf3f
Leigh Hunt <leigh@venari.co.nz>
2016-11-21 23:58:52 +1300

    Successfully posting data now

--- accelerometer.js
+++ accelerometer.js
@@ -8,21 +8,15 @@ of x, y, and z data from the accelerometer
 
 var tessel = require('tessel');
 var accel = require('accel-mma84').use(tessel.port['A']);
-var http = require('http');
-
-/*var options = {
-    host: 'fluff-protocol.hyperdev.space',
-    port: 443,
-    path: '/data',
-    method: 'POST',
-};*/
-
+var request = require('request');
+/*
 var options = {
     host: 'requestb.in',
     port: 80,
     path: '/1albc7s1',
     method: 'POST',
 };
+*/
 
 function oneInEvery(number, every) {
   return !(number % every);
@@ -34,42 +28,38 @@ var counter = 0;
 accel.on('ready', function () {
 	// Stream accelerometer data
 	accel.on('data', function (xyz) {
-		
-		var data = [
-		  {
+
+		var data = {
 			x: xyz[0].toFixed(2),
 			y: xyz[1].toFixed(2),
 			z: xyz[2].toFixed(2),
 			id: 'tessel'
-		  }
-		]
+    };
+
 		counter++;
 		if(oneInEvery(counter, 100))
 		{
 			console.log('Sending http request');
-			
-			http.request(options).end(JSON.stringify(data));
-			/*
-			var request = http.request(options, function(res)
-			{
-				// Print out data when we get it
-				res.on('data', function(data)
-				{
-					console.log('response data', data);
-				});
-			});
-			 
-			// Write any POST data 
-			request.write(JSON.stringify(data));
-			 */
+      console.log(JSON.stringify(data));
+
+      var options = {
+        uri: 'https://fluff-protocol.hyperdev.space/data',
+        method: 'POST',
+        json: data
+      };
+
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log('OK');
+        }
+      });
+
 			console.log('x:', xyz[0].toFixed(2),'y:', xyz[1].toFixed(2),'z:', xyz[2].toFixed(2));
 		}
-		
-		
 
 	});
 
 	accel.on('error', function(err){
-	console.log('Error:', err);
+	   console.log('Error:', err);
 	});
 });
